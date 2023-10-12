8bda87618ca02462ba1012d22e927e44bc6d6cc6
松本直樹 <nmatsumoto@matsumotonaokinoMacBook-Pro.local>
2023-10-12 19:47:49 +0900

    SMUI導入

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import "../theme/smui.css";
+</script>
+
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+  />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700"
+  />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Roboto+Mono"
+  />
+  <title>GoNOW</title>
+</svelte:head>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
+<script lang="ts">
+  import Textfield from "@smui/textfield";
+  import HelperText from "@smui/textfield/helper-text";
+
+  let value = "";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<Textfield bind:value label="Label">
+  <HelperText slot="helper">Helper Text</HelperText>
+</Textfield>
